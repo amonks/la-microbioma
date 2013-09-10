36aa25f1025a86943f29aa87c294f453409b7541
audy <harekrishna@gmail.com>
2013-09-10 17:57:39 -0400

    el primero compromiso

---
+++ play_microbiome.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+
+require 'bundler'
+Bundler.require
+
+NOTE_MAP = {
+  0 => ' ',
+  1 => 'a',
+  2 => 'b',
+  3 => 'c',
+  4 => 'd',
+  5 => 'e',
+  6 => 'f',
+  7 => 'g'
+}
+
+bleep = Bloops.new
+bleep.tempo = 120
+instrument = bleep.sound Bloops::SINE
+
+File.open ARGV[0] do |handle|
+  handle.gets # skip header
+  handle.each do |line|
+    counts = line.strip.split(',')[1..-1].map(&:to_i)
+
+    song = counts.map { |x| NOTE_MAP[x] }.join(' ')
+
+    bleep.tune instrument, song
+    bleep.play
+
+    sleep 1 while !bleep.stopped?
+  end
+end
---
+++ readme.md
@@ -0,0 +1,20 @@
+# la microbioma
+
+¡Eschucho la hermosa múscia!
+
+Austin G. Davis-Richardson
+
+## Instrucciones
+
+1. Instalar
+
+```
+cd player_microbiome/
+bundle
+```
+
+2. Tocar la microbioma!
+
+```
+./jugar.rb otus.csv
+```
